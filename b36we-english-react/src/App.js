0305150a45e229fe005cc4bfa86955662dbab9b1
Madhaveeswara V <madhaveeswaraleo@gmail.com>
2022-08-21 11:59:37 +0530

    Modified App

--- b36we-english-react/src/App.js
+++ b36we-english-react/src/App.js
@@ -4,20 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
      
       <Msg name="Madhav" pic="pic2.jpg" />
       <Msg name="Max" pic="pic1.png" />
@@ -31,7 +17,7 @@ function Msg({ name, pic }) {
   return (
     <div>
       <h1>Hello {name}</h1>
-      <img src={pic} alt="Girl in a jacket" width="150" height="150"></img>
+      <img src={pic} alt="Hypothetical Alt text" width="150" height="150"></img>
     </div>
   );
 }
